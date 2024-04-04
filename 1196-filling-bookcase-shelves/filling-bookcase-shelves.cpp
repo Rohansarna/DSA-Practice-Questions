700c16abb561d9584f7861a0ee7f13b5b3757b99
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2024-04-04 13:34:03 +0530

    Time: 12 ms (5.99%) | Memory: 31.3 MB (5.18%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-   int solve(vector<vector<int>> &books, int i, int remShelfWidth, int maxShelfHeight, 
-			 int shelfWidth, vector<vector<int>> &dp) {
+vector<vector<int>> dp;
+    int solve(int i , int max_height , int rem_width , int shelfwidth , vector<vector<int>> & v ){
+        if( i== v.size()){
+            return max_height; 
+        }
+        if(dp[i][rem_width]!= -1 ){
+            return dp[i][rem_width]; 
+        }
+        int curr_shelf = INT_MAX , next_shelf = INT_MAX; 
+        int book_width = v[i][0]; 
+        int book_height = v[i][1]; 
+        if(book_width <= rem_width ){
+            curr_shelf = solve(i + 1 , max(max_height , book_height) , rem_width - book_width , shelfwidth , v ); 
+        }
+        next_shelf =  max_height +  solve(i + 1 , book_height , shelfwidth - book_width , shelfwidth , v ); 
 
-		if(i == books.size()) {
-			return maxShelfHeight;
-		}
+        return dp[i][rem_width] = min(curr_shelf , next_shelf); 
 
-		if(dp[i][remShelfWidth] != -1)
-			return dp[i][remShelfWidth];
-
-		int currShelf = INT_MAX, nextShelf = INT_MAX;
-
-		int bookWidth = books[i][0];
-		int bookHeight = books[i][1];
-
-		if(bookWidth <= remShelfWidth) {
-			currShelf = solve(books, i + 1, remShelfWidth - bookWidth, 
-							  max(maxShelfHeight, bookHeight), shelfWidth, dp);
-		}
-
-		nextShelf = maxShelfHeight + solve(books, i + 1, shelfWidth - bookWidth, 
-										   bookHeight, shelfWidth, dp);
-
-		return dp[i][remShelfWidth] = min(currShelf, nextShelf);
-	}
-public:
-	int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-		vector<vector<int>> dp(1001, vector<int> (shelfWidth + 1, -1));
-		return solve(books, 0, shelfWidth, 0, shelfWidth, dp);
-	}
+    }
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+         dp.resize(1001, vector<int> (shelfWidth + 1, -1));
+        return solve( 0 , 0 , shelfWidth , shelfWidth , books); 
+    }
 };
