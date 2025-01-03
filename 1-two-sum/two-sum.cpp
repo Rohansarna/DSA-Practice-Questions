84c19594df1c2ec284270ff5ec6ebf535e6df8f0
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-03 15:45:27 +0530

    Time: 7 ms (51.53%) | Memory: 16.2 MB (7.63%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res ; 
+        map<int ,int> x ; 
         int n = nums.size(); 
         for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                int x  = nums[i] + nums[j]; 
-                if(x == target ){
-                    res.push_back(i);
-                    res.push_back(j);
-                    break; 
-                }
-            }
+            x.insert({nums[i] , i }); 
         }
-        return res; 
+        for(int i = 0 ; i<nums.size() ;i++){
+            int y = target - nums[i]; 
+            if(x.find(y) != x.end() && x[y] !=  i ){
+                return {i , x[y]}; 
+            } 
+        }
+        return {0 , 0}; 
+
+
+
     }
 };
+
+
+// Brute Force
+// vector<int> twoSum(vector<int>& nums, int target) {
+//     vector<int> res;
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             int x = nums[i] + nums[j];
+//             if (x == target) {
+//                 res.push_back(i);
+//                 res.push_back(j);
+//                 break;
+//             }
+//         }
+//     }
+//     return res;
+// }
