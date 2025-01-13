1be388878e1ca3966515482615f8eff71a45c991
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-13 15:56:33 +0530

    Time: 32 ms (53.38%) | Memory: 65.2 MB (53.52%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,60 +10,21 @@
  */
 class Solution {
 public:
-      void Mergesorting(ListNode **head ){
-        ListNode *curr= *head ; 
-        if(!curr || !curr ->next ){
-            return ; 
-        }
-        ListNode *first = 0   ; 
-         ListNode *second  = 0 ; 
-       FindMiddle(curr, &first , & second ); 
-        Mergesorting(&first); 
-        Mergesorting(&second); 
-       *head = mergeboth(first , second ); 
-     
-    }
-    void FindMiddle(ListNode *curr , ListNode **first , ListNode **second){
-        
-        ListNode *slow = curr; 
-        ListNode *fast  = curr->next;
-        while(fast != 0 ){
-            fast = fast ->next ; 
-            if(fast != 0 ){
-                slow = slow ->next ; 
-                fast = fast ->next; 
-            }
-        } 
-        *first = curr; 
-        *second = slow ->next; 
-        slow ->next =  0 ; 
-    }
-    ListNode *mergeboth(ListNode *first , ListNode *second ){
-        if(!first){
-            return second ; 
-        }
-        if(!second){
-            return first;
-        }
-        ListNode *answer = 0 ; 
-        if(first ->val <= second ->val){
-            answer = first; 
-            answer ->next = mergeboth(first ->next , second ); 
-        }
-        else{
-              answer = second; 
-            answer ->next = mergeboth(first  , second ->next);
-        }
-
-
-
-return answer; 
-
-
-    }
-
     ListNode* sortList(ListNode* head) {
-         Mergesorting(&head); 
-        return head; 
+        vector<int> v ; 
+        ListNode *t = head ; 
+        while(t){
+            v.push_back(t->val); 
+            t = t->next; 
+        }
+        sort(v.begin() , v.end()); 
+        ListNode * res = new ListNode(0);
+        ListNode *curr = res;  
+        for(int i = 0 ; i<v.size() ;i++){
+            ListNode *x = new ListNode(v[i]); 
+            curr ->next = x ; 
+            curr = curr ->next ; 
+        }
+        return res->next; 
     }
 };
