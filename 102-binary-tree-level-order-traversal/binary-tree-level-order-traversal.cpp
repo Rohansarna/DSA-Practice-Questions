f992685a3d27486248092daa3afc2f95a9ba8fd1
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-15 14:45:59 +0530

    Time: 3 ms (27.07%) | Memory: 17.1 MB (36.96%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,34 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-      vector<vector<int>> levelOrder(TreeNode* root) {
-        if(!root ){
-            return {}; 
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) {
+            return {};
         }
-        queue<TreeNode*>q; 
-        q.push(root); 
-        vector<vector<int>> res ;
-        while(!q.empty()){
-            int size = q.size(); 
-            vector<int> v ;
-            for(int i = 0 ; i< size ; i++){
-                TreeNode*t  = q.front(); 
-                q.pop(); 
-                v.push_back(t ->val); 
-                if(t->left){
-                    q.push(t->left); 
+        vector<vector<int>> res;
+        TreeNode* x = root;
+        queue<TreeNode*> q;
+        q.push(x);
+        while (!q.empty()) {
+        vector<int> temp;
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+
+                temp.push_back(q.front()->val);
+                if (q.front()->left) {
+                    q.push(q.front()->left);
                 }
-                if(t ->right ){
-                    q.push(t ->right); 
+                if (q.front()->right) {
+                    q.push(q.front()->right);
                 }
+                q.pop();
             }
-            res.push_back(v); 
+
+            res.push_back(temp);
         }
-        return  res ; 
+        return res;
     }
 };
