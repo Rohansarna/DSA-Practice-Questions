6005e8800beb85efe3f4e599f0f9a0a1f7fb46c6
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-20 18:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (42.49%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -13,16 +13,21 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == 0 && q == 0) {
-            return 1;
+        // If both nodes are NULL, they are identical
+        if (p == NULL && q == NULL) {
+            return true;
         }
-        if (p == 0 || q == 0) {
-            return 0;
+        // If only one of the nodes is NULL, they are not identical
+        if (p == NULL || q == NULL) {
+            return false;
         }
+        // Check if values are equal and recursively check left and right
+        // subtrees
         if (p->val == q->val) {
-            return isSameTree(p ->left , q ->left) && isSameTree(p ->right , q ->right); 
-        } else {
-            return 0 ; 
+            return isSameTree(p->left, q->left) &&
+                   isSameTree(p->right, q->right);
         }
+        // Values are not equal, they are not identical
+        return false;
     }
 };
