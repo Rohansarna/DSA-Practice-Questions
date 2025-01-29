85d251245d8f7bfad2e7bfe6c200e4d60e4ce054
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-29 18:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (41.44%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& arr, int n) {
-        vector<int> flowerbed = arr;
-          int size = flowerbed.size() ;
-          if(n ==0 ){
-            return 1; 
-          }
-          if(size == 0){
-                return 0 ; 
-          }
-          if(size == 1){
-            return flowerbed[0] == 0 && n == 1  ? 1 : 0 ; 
-          }
-    int count = 0;
-    if (flowerbed[0] == 0 && flowerbed[1] == 0)
-    {
-        flowerbed[0] = 1;
-        count += 1;
-    }
-
-    if (flowerbed[size-1] == 0 && flowerbed[size - 2] == 0)
-    {
-        flowerbed[size-1] = 1;
-        // n-= 1  ;
-        count += 1;
-    } 
-    for(int i = 0 ; i < size-3 ;i++ ){
-        if(flowerbed[i] ==0 && flowerbed[i + 1] == 0 && flowerbed[i+2] == 0){
-            count++; 
-            flowerbed[i + 1] =1; 
+    bool canPlaceFlowers(vector<int>& flower, int n) {
+        int size = flower.size();
+        if (size == 1) {
+            if (n == 1) {
+                return flower[0] == 0;
+            }
+            return n == 0;
         }
-    }
-    return count >= n ? 1 : 0 ; 
 
+        if (flower[0] == 0 && flower[1] == 0) {
+            flower[0] = 1;
+            n--;
+        }
+        for (int i = 1; i < size - 1;) {
+            if (n <= 0) {
+                return 1;
+            }
+            if (flower[i - 1] == 0 && flower[i] == 0 && flower[i + 1] == 0) {
+                n--;
+                flower[i] = 1;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        // cout<< n << " ";
+        if (flower[size - 1] == 0 && flower[size - 2] == 0) {
+            flower[size - 1] = 1;
+            n--;
+        }
+        return n <= 0;
     }
 };
