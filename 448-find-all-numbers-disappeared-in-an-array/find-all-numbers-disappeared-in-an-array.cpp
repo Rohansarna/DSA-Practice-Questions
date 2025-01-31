f3dade574e534862b13c098f4b2e7dca5969fad2
Rohan Sarna <117627996+Rohansarna@users.noreply.github.com>
2025-01-31 16:39:00 +0530

    Time: 3 ms (81.39%) | Memory: 53.1 MB (55.42%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int N = *max_element(nums.begin() , nums.end()); 
-        int n = nums.size(); 
-        vector<int>v(n+1 , 0 ); 
-        for(int i = 0 ; i < n ; i++){
-            v[nums[i]]++; 
+        vector<int> results;
+        
+        // Set contents of all indices (minus 1) that appear in the array to be negative
+        for (int i = 0; i < nums.size(); ++i)
+        {
+			// Note that we have to use the absolute value in the next 2 lines, to avoid trying to access a negative index in some cases
+            if (nums[abs(nums[i]) - 1] > 0)
+                nums[abs(nums[i]) - 1] *= -1;
         }
-
-        vector<int> res; 
-        for(int i = 1 ; i < n+1 ; i++){
-            if(v[i] == 0 ){
-                res.push_back(i); 
-            }
-        }
-        return res; 
+        
+        // A positive element means that the element (index + 1) does not appear in the array, so save it
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] > 0)
+                results.push_back(i + 1);
+        
+        return results;
     }
 };
